3f01e511212e211f91f38c9ce0798c6c842073dd
Andras Nemes <andras.nemes@iongroup.com>
2025-01-29 22:07:12 +0100

    scan networks on UI

---
+++ dev-server/index.ts
@@ -0,0 +1,48 @@
+import express, { Express } from 'express'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+
+const app: Express = express()
+
+const PORT = 1234
+
+
+// // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    console.log(`${req.method} ${req.url}`)
+    const indexHtmlContent = fs.readFileSync('index.html', 'utf8')
+    res.set('Content-Type', 'text/html')
+    res.send(Buffer.from(indexHtmlContent))
+})
+
+app.get('/scan-wifi', (req, res) => {
+    (req as any).socket = null
+    console.log(`${req.method} ${req.url}`)
+    const response = {
+        "networks": [
+            { "SSID": "Rem-Guest", "RSSI": -68, "EncryptionType": "WPA2/PSK" },
+            { "SSID": "Rem-IOT", "RSSI": -68, "EncryptionType": "WPA2/PSK" },
+            { "SSID": "Telekom-992531", "RSSI": -78, "EncryptionType": "WPA2/PSK" },
+            { "SSID": "Telekom-fb562f", "RSSI": -83, "EncryptionType": "Unknown" },
+            { "SSID": "Telekom-fb562f", "RSSI": -83, "EncryptionType": "None" },            
+            { "SSID": "Rem-IOT", "RSSI": -85, "EncryptionType": "WPA2/PSK" },
+            { "SSID": "Rem-Guest", "RSSI": -86, "EncryptionType": "WPA2/PSK" }
+        ]
+    }
+
+    res.json(response)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`***************************************************** `)
+    console.log(`* dev - server running                *`)
+    console.log(`* at http://localhost:${PORT}   *`)
+    console.log(`*****************************************************`)
+
+})
